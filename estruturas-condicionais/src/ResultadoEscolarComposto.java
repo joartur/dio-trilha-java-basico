10c2a782a9012e52ffb3db0a448eb064839ecb77
Joamerson Artunes <145156905+joartur@users.noreply.github.com>
2024-10-11 17:22:23 -0300

    Add files via upload

---
+++ estruturas-condicionais/src/ResultadoEscolarComposto.java
@@ -0,0 +1,14 @@
+// Condicional Composta
+
+public class ResultadoEscolarComposto {
+    
+    public static void main(String[] args) {
+        int nota = 8;
+
+        if(nota >= 7)
+        System.out.println("Aprovado");
+
+        else
+        System.out.println("Reprovado");
+    }
+}
